86915c6e171d5a7ddf3e21ef31a8ce4c0bd8940a
Supermangitsun <635600284@qq.com>
2019-08-19 20:51:12 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUi from 'element-ui' // 引入elementui组件
-import './assets/nomoless/index.less'
+import './assets/nomoless/index.less' // 引入基础的base样式
 Vue.use(ElementUi) // 注册elementui组件
 Vue.config.productionTip = false
 
