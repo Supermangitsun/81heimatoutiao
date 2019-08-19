c87976e1d9668c75e7e28a577e51273fb5ecd1eb
Supermangitsun <635600284@qq.com>
2019-08-19 20:10:18 +0800

    组件背景图片的实现

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import './assets/nomoless/index.less'
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/login/index.vue
@@ -0,0 +1,17 @@
+<template> <!-- vue实例外创建 -->
+    <div class="login">
+
+    </div>
+</template>
+<!-- 调用   <mycom></mycom> -->
+<script>
+
+</script>
+<style lang="less" scoped>
+.login{
+    width: 100%;
+    height: 100vh;
+    background-image: url('../../assets/img/login_bg.jpg');
+    background-size: cover;
+}
+</style>
