cd2d452050723096ca02618d00dd0bce20844277
Supermangitsun <635600284@qq.com>
2019-08-19 20:35:54 +0800

    注册elementui组件

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUi from 'element-ui' // 引入elementui组件
 import './assets/nomoless/index.less'
+Vue.use(ElementUi) // 注册elementui组件
 Vue.config.productionTip = false
 
 new Vue({
