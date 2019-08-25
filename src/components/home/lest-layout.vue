99ad2054e8d71e741f28a255374ba723740018ff
Supermangitsun <635600284@qq.com>
2019-08-25 22:02:09 +0800

    主页左侧分栏及布局

---
+++ src/components/home/lest-layout.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <div class="nav">
+            <img src="../../assets/img/logo_admin.png" alt="">
+        </div>
+         <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <!-- el-submenu导航折叠 -->
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>导航一</span>
+        </template>
+       <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">导航二</span>
+      </el-menu-item>
+      </el-submenu>
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">导航二</span>
+      </el-menu-item>
+      <el-menu-item index="3" disabled>
+        <i class="el-icon-document"></i>
+        <span slot="title">导航三</span>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <i class="el-icon-setting"></i>
+        <span slot="title">导航四</span>
+      </el-menu-item>
+    </el-menu>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style lang='less' scoped>
+    .nav{
+        height: 80px;
+        width: 100%;
+        background-color: #2e2f32;
+        text-align: center;
+        padding: 15px 0;
+        img{
+            width: 50%;
+        }
+    }
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,34 @@
 <template> <!-- vue实例外创建 -->
-    <div>
-        home
-    </div>
+        <!--大容器  -->
+    <el-container>
+        <!-- 左侧区域 -->
+        <el-aside class="left">
+            <lest-layout></lest-layout>
+        </el-aside>
+    <!-- 嵌套容器 -->
+       <el-container>
+           <!-- 上部区域 -->
+            <el-header>头部区域</el-header>
+            <!-- 中间区域 -->
+            <el-main>中间区域</el-main>
+       </el-container>
+    </el-container>
 </template>
 <script>
+// 引用左侧布局组件
+import lestlayout from '../../components/home/lest-layout'
+export default {
+  // 注册左面布局组件
+  components: {
+    'lest-layout': lestlayout
+  }
+}
 
 </script>
 <style lang='less' scope>
-
+.left{
+    width: 200px;
+    height: 100vh;
+    background-color: #323745;
+}
 </style>
